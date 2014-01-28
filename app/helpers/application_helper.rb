9b0f82292f9a0b6ea2e21fa766287453677dc377
Susie Ye <susie.ye19@gmail.com>
2014-01-27 22:17:30 -0800

    Format CodeRay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def coderay(text)
+		notes = text
+		loops = notes.scan(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m).length
+
+		loops.times {
+			notes.sub!(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m) do
+	      content_tag("notextile", CodeRay.scan($2, $1).div(:css => :class).html_safe)
+			end
+		}
+		return notes
+	end
 end
