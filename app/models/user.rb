b2fbc2ae8124ecabc7b140bd4117835d09e981e2
Alex Yang <alexyang.personal@gmail.com>
2014-01-30 15:51:55 -0800

    Add discount code

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,32 @@ class User < ActiveRecord::Base
     Stripe.api_key = ENV["STRIPE_API_KEY"]
     token = stripe_card_token
 
-    # Create the charge on Stripe's servers - this will charge the user's card
-    begin
-      charge = Stripe::Charge.create(
-        :amount => 9900,
-        :currency => "usd",
-        :card => token,
-      )
-      # flash[:success] = "Thanks for ordering!"
-    rescue Stripe::CardError => e
-      # flash[:danger] = e.message
+    if email == 'julian.liang.wh10@wharton.upenn.edu'
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          :amount => 4900,
+          :currency => "usd",
+          :card => token,
+          :description => "Charge for #{email}"
+        )
+        # flash[:success] = "Thanks for ordering!"
+      rescue Stripe::CardError => e
+        # flash[:danger] = e.message
+      end
+    else
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          :amount => 9900,
+          :currency => "usd",
+          :card => token,
+          :description => "Charge for #{email}"
+        )
+        # flash[:success] = "Thanks for ordering!"
+      rescue Stripe::CardError => e
+        # flash[:danger] = e.message
+      end
     end
   end
 end
