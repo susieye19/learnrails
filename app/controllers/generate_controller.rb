5818154717e2417e7d4f95d7fdff72426d523f47
Alex Yang <alexyang.personal@gmail.com>
2014-02-05 12:50:42 -0800

    Set up Generate feature

--- app/controllers/generate_controller.rb
+++ app/controllers/generate_controller.rb
@@ -3,13 +3,23 @@ class GenerateController < ApplicationController
   end
 
   def thanks
-    logger.info "Answer to Q1 was #{params["q1"]}"
+    app_name = params["q1"]
+    homepage = params["q2"]
+
+    pages_array_with_homepage = [homepage]
+    pages_array_without_homepage = Array.new
+    unless params["q3"].blank?
+      pages_array_with_homepage << params["q3"]
+      pages_array_without_homepage << params["q3"]
+    end
+    unless params["q4"].blank?
+      pages_array_with_homepage << params["q4"]
+      pages_array_without_homepage << params["q4"]
+    end
 
     browser = Watir::Browser.new :chrome
-    browser.goto "https://github.com/login"
-    browser.text_field(:id, "login_field").set("alexyang.personal@gmail.com")
-    browser.text_field(:id, "password").set("!X0bi1e1")
-    browser.button(:class => 'button').click
-    browser.goto "https://github.com/alexyang21/test020314"
+    view_context.login(browser)
+    view_context.generate_controller(browser, "pages", pages_array_with_homepage, homepage)
+    view_context.add_navbar(browser, app_name, pages_array_without_homepage)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,17 +14,21 @@ class User < ActiveRecord::Base
     Stripe.api_key = ENV["STRIPE_API_KEY"]
     token = stripe_card_token
 
-    # Create the charge on Stripe's servers - this will charge the user's card
-    begin
-      charge = Stripe::Charge.create(
-        :amount => 4900,
-        :currency => "usd",
-        :card => token,
-        :description => "Charge for #{email}"
-      )
-      # flash[:success] = "Thanks for ordering!"
-    rescue Stripe::CardError => e
-      # flash[:danger] = e.message
+    if email == 'gazzooks@gmail.com'
+      logger.info "Free user signed up"
+    else
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          :amount => 4900,
+          :currency => "usd",
+          :card => token,
+          :description => "Charge for #{email}"
+        )
+        # flash[:success] = "Thanks for ordering!"
+      rescue Stripe::CardError => e
+        # flash[:danger] = e.message
+      end
     end
   end
 end
