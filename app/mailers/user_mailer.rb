20c2f9031e27600ced1c666648fcf93289479ba9
Alex Yang <alexyang.personal@gmail.com>
2014-03-05 00:11:31 -0800

    Change sender name to BaseRails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "Alex Yang <alex@baserails.com>"
+  default from: "BaseRails <alex@baserails.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
