1df89d1833925b6634190c7b5039c69478d84af5
Susie Ye <susie.ye19@gmail.com>
2014-04-09 21:02:30 -0400

    Format thanks page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    dashboard_path
+    chapters_path
   end
 
   # def configure_permitted_parameters
