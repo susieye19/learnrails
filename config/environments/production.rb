561d2ef6149598612898602936a26f6185f04bf2
Alex Yang <alexyang.personal@gmail.com>
2014-04-24 06:15:57 -0400

    Set up MailGun email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Learnrails::Application.configure do
 
   # Required for Devise. REMEMBER TO CHANGE TO ACTUAL HOST
   config.action_mailer.default_url_options = { :host => '' }
+
+  # MailGun email configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+       :authentication => :plain,
+       :address => "smtp.mailgun.org",
+       :port => 587,
+       :domain => "mg.baserails.com",
+       :user_name => "postmaster@mg.baserails.com",
+       :password => ENV["MG_PASSWORD"]
+  }
 end
