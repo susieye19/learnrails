ff2e829a7409259599723c4be3fdd17c19f3024a
Alex Yang <alexyang.personal@gmail.com>
2014-05-10 19:49:20 -0400

    Remove multiple application layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,27 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Select which layout to use based on whether user is signed in
-  layout :layout_by_resource
-
-  helper_method :currently_signed_in_user
-
   protected
 
   def after_sign_in_path_for(resource)
     chapters_path
-  end
-
-  def layout_by_resource
-    if user_signed_in?
-      "application"
-    else
-      "application_signed_out"
-    end
-  end
-
-  def currently_signed_in_user
-    return current_user
   end
 
   # def configure_permitted_parameters
