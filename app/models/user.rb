2cee3c9a0f53ad1db9d1926348a631613e551c9c
Alex Yang <alexyang.personal@gmail.com>
2014-05-19 17:57:08 -0400

    Change stripe_card_token column to virtual attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :stripe_card_token
 
   has_many :questions, dependent: :destroy
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513045523) do
+ActiveRecord::Schema.define(version: 20140519215159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 20140513045523) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
-    t.string   "stripe_card_token"
     t.string   "name"
     t.string   "coupon"
     t.decimal  "amount"
