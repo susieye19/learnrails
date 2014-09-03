0f7f5a8062d807f4e5c0a7ee3aaf2cec5e6ac441
Alex Yang <alexyang.personal@gmail.com>
2014-09-03 17:51:57 -0400

    Remove some debugging comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,6 @@
 module ApplicationHelper
 	def coderay(text)
 		notes = text
-		# scanned = notes.scan(/\<code(?: lang="(.+?)")\>(.+?)\<\/code\>/m)
-		# puts "Scanned is #{scanned}"
-
-		# indices = []
-		# scanned.each_with_index do |scan, index|
-		# 	unless scan[0].blank?
-		# 		indices.push(index)
-		# 	end
-		# end
-		# puts "indices is #{indices}"
 
 		loops = notes.scan(/\<code(?: lang="(.+?)")\>(.+?)\<\/code\>/m).length
 
@@ -19,7 +9,6 @@ module ApplicationHelper
 	      content_tag("notextile", CodeRay.scan($2, $1).div(:css => :class).html_safe)
 			end
 		}
-		# puts "Notes is #{notes}"
 		return notes
 	end
 end
