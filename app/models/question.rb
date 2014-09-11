e86110dce05135bc9b3647c74eeb8857e873965a
Alex Yang <alexyang.personal@gmail.com>
2014-09-11 17:10:02 -0400

    Add slug to ask a developer questions URLs

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,14 @@
 class Question < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :subject, use: [:slugged, :history]
+  
   acts_as_commentable
 
   belongs_to :user
 
   validates :subject, presence: true
+  
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
