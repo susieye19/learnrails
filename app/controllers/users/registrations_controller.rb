02f80d9e5615245cda5a48402ec6452d769ab3f1
Alex Yang <alexyang.personal@gmail.com>
2014-12-08 01:40:36 -0500

    Add invoice payment succeeded webhook and user email notifications

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,9 +40,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
         clean_up_passwords resource
         respond_with resource
       end
+      
     else
 
-      if resource.save_with_payment
+      if resource.save && resource.save_with_payment
         # Send new subscription email notification
         UserMailer.new_subscription(resource.name, resource.email, @plan).deliver
 
@@ -60,8 +61,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
         clean_up_passwords resource
         respond_with resource
       end
+      
     end
-    
   end
 
   def subscribe
